46ac90728a4099e98aa7b9fd2a8b9e0daa16c21f
Shekhar saini <sainishekhar581@gmail.com>
2019-12-08 00:41:20 +0530

    Updates

---
+++ precache-manifest.351e063606a83255696cb00e5fea1467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da6bb9c39f2fb3b51672152946634f4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f7f4e281238f81362d55",
+    "url": "/monsters-rolodex/static/css/main.d6581f98.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "f7f4e281238f81362d55",
+    "url": "/monsters-rolodex/static/js/main.9ba594e0.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
